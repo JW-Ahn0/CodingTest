7dec840bf907e6827dfbcd80ddaad62837a09672
JW-Ahn0 <ajwquestion@gmail.com>
2024-02-08 11:45:35 +0900

    Soved.ac Class2

---
+++ C_10828/Main.java
@@ -0,0 +1,50 @@
+package C_10828;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        Stack<Integer> st = new Stack<>();
+        StringBuilder sb= new StringBuilder();
+        for(int i = 0 ; i <t;i++){
+            String temp = sc.nextLine();
+            String[] str = temp.split(" ");
+            if(str[0].equals("push")){
+                st.push(Integer.valueOf(str[1]));
+            }
+            else if(str[0].equals("pop")){
+                if(st.isEmpty()){
+                    sb.append("-1\n");
+                }
+                else{
+                    sb.append(st.pop()+"\n");
+                }
+            }
+            else if(str[0].equals("size")){
+                sb.append(st.size()+"\n");      
+            }
+            else if(str[0].equals("empty")){
+                if(st.isEmpty()){
+                    sb.append("1\n");
+                }
+                else{
+                    sb.append("0\n");
+                }                
+            }
+            else if(str[0].equals("top")){
+                if(st.isEmpty()){
+                    sb.append("-1\n");
+                }
+                else{
+                    sb.append(st.peek()+"\n");
+                }                
+            }                    
+        }
+        System.out.println(sb.toString());
+        sc.close();
+    }
+}
