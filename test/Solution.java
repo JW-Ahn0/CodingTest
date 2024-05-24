3265511643b450aa89e72c9bbcd1537ce335e0f8
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-05-24 16:35:32 +0900

    [Feat] 0525

--- test/Solution.java
+++ test/Solution.java
@@ -1,7 +1,13 @@
+import java.util.*;
+
 class Solution
 {
  public static void main(String args[]) throws Exception
  {
+  Scanner sc = new Scanner(System.in);
+
    System.out.println("Hello World");
- }
+
+  sc.close();
+}
 }
