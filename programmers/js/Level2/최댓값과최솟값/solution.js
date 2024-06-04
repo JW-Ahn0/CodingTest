88ff218a07174952f935464cf2a9eca698e24194
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-06-04 21:53:39 +0900

    [Feat] 0604

---
+++ programmers/js/Level2/최댓값과최솟값/solution.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = '';
+    const arr = s.split(' ');
+    var min = Math.min(...arr);
+    var max = Math.max(...arr);
+    answer =min +' ' + max;
+    return answer;
+}
